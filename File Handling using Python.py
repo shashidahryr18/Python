286ea6f7b1a960605abd9343927845df3b25286b
SHASHIDHAR Y R <shashidhargowda579@gmail.com>
2026-07-24 18:32:26 +0530

    Rename lab13.py to File Handling using Python.py

--- File Handling using Python.py
+++ File Handling using Python.py
@@ -1,66 +1,65 @@
-file1 = open("C:/Users/Shreyas N/Downloads/ict.txt")
+file1 = open("C:/Users/Shashidhar/Downloads/ict.txt")
 
-file1 = open(r"C:/Users/Shreyas N/Downloads/ict.txt")
+file1 = open(r"C:/Users/Shashidhar/Downloads/ict.txt")
 
-#THis will print every line one by one in the file
+# This will print every line one by one in the file
 for i in file1:
-    print (i)
+    print(i)
 
-file1 = open(r"C:/Users/Shreyas N/Downloads/ict.txt") 
-print (file1.read())
+file1 = open(r"C:/Users/Shashidhar/Downloads/ict.txt")
+print(file1.read())
 
-with open(r"C:/Users/Shreyas N/Downloads/ict.txt",'r') as f1:  
-    data = f1.read() 
+with open(r"C:/Users/Shashidhar/Downloads/ict.txt", 'r') as f1:
+    data = f1.read()
 print(data)
 
-f1 = open(r"C:/Users/Shreyas N/Downloads/ict.txt")
-print (f1.read(5))
+f1 = open(r"C:/Users/Shashidhar/Downloads/ict.txt")
+print(f1.read(5))
 
-with open(r"C:/Users/Shreyas N/Downloads/ict.txt",'r') as file:
+with open(r"C:/Users/Shashidhar/Downloads/ict.txt", 'r') as file:
     data = file.readlines()
     for line in data:
         word = line.split()
-        print (word)
+        print(word)
 
-print (word)
- 
-file = open("3EK2A.txt",'w')
+print(word)
+
+file = open("3EK2A.txt", 'w')
 file.write("ICT ICT ICT \n")
 file.write("ICT ICT ICT ICT ICT")
 file.close()
 
-with open("Marwadi_university.txt", "w") as f: 
-    f.write("Hello kitty!!!") 
-    f.close()
+with open("Marwadi_university.txt", "w") as f:
+    f.write("Hello kitty!!!")
 
-file = open("3EK2A.txt",'a')
+file = open("3EK2A.txt", 'a')
 file.write("\n Department Department")
 file.close()
 
-with open(r'C:/Users/Shreyas N/Downloads/a.tif', 'rb') as file:
+with open(r'C:/Users/Shashidhar/Downloads/a.tif', 'rb') as file:
     binary_data = file.read()
 
-with open('C:/Users/Shreyas N/Downloads/a.tif', 'wb') as f:
+with open(r'C:/Users/Shashidhar/Downloads/a.tif', 'wb') as f:
     f.write(binary_data)
-    f.close()
-    
+
 import csv
+
 # Reading from a CSV file
-with open('C:/Users/Shreyas N/Downloads/data-1.csv', 'r') as file:
+with open(r'C:/Users/Shashidhar/Downloads/data-1.csv', 'r') as file:
     reader = csv.reader(file)
     for row in reader:
         print(row)
 
 import csv
+
 with open('output.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['Name', 'Subject', 'Mark'])
     writer.writerow(['Aansh', 'PWP', 9])
     writer.writerow(['Ashutosh', 'PWP', 10])
-    file.close()
 
-#postlab
- 
+# Post Lab
+
 def count_file_contents(filename):
     lines = 0
     words = 0
@@ -73,46 +72,49 @@ def count_file_contents(filename):
     print(f"Lines: {lines}")
     print(f"Words: {words}")
     print(f"Characters: {characters}")
+
 if __name__ == "__main__":
-    filepath = r"C:/Users/Shreyas N/Downloads/ict.txt"
+    filepath = r"C:/Users/Shashidhar/Downloads/ict.txt"
     count_file_contents(filepath)
-        
+
 def read_file_to_list(filename):
     lines_list = []
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
-            lines_list.append(line.rstrip('\n'))  # Remove trailing newline characters
+            lines_list.append(line.rstrip('\n'))
     return lines_list
+
 if __name__ == "__main__":
-    filepath = r"C:/Users/Shreyas N/Downloads/ict.txt"
+    filepath = r"C:/Users/Shashidhar/Downloads/ict.txt"
     lines = read_file_to_list(filepath)
     print(lines)
 
 import csv
+
 def read_csv_and_print(filename):
     with open(filename, 'r', encoding='utf-8') as csvfile:
         csvreader = csv.reader(csvfile)
         for row in csvreader:
             print(row)
+
 if __name__ == "__main__":
-    filepath = r"C:/Users/Shreyas N/Downloads/data-1.csv"
+    filepath = r"C:/Users/Shashidhar/Downloads/data-1.csv"
     read_csv_and_print(filepath)
-    
+
 def merge_files(file1, file2, merged_file):
     with open(merged_file, 'w', encoding='utf-8') as outfile:
-        # Write contents of file1
         with open(file1, 'r', encoding='utf-8') as f1:
             for line in f1:
                 outfile.write(line)
-        # Write contents of file2
+
         with open(file2, 'r', encoding='utf-8') as f2:
             for line in f2:
                 outfile.write(line)
 
 if __name__ == "__main__":
-    file1_path = r"C:/Users/Shreyas N/Downloads/ict.txt"
-    file2_path = r"C:/Users/Shreyas N/Downloads/ict.txt"
-    merged_file_path = r"C:/Users/Shreyas N/Downloads/merged.txt"
-    
+    file1_path = r"C:/Users/Shashidhar/Downloads/ict.txt"
+    file2_path = r"C:/Users/Shashidhar/Downloads/ict.txt"
+    merged_file_path = r"C:/Users/Shashidhar/Downloads/merged.txt"
+
     merge_files(file1_path, file2_path, merged_file_path)
     print(f"Files merged into {merged_file_path}")
